0ebbda085e01462a49c6aa6f8bfd84a987f05ba0
jamarsto <jason@jasonmarston.me.uk>
2026-04-26 11:17:45 +0100

    Updated plugin versions and cleaned up root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,12 @@ plugins {
     id("io.freefair.lombok") version "9.2.0" apply false
     id("org.kordamp.gradle.jandex") version "2.3.0" apply false
     id("org.gradlex.extra-java-module-info") version "1.14" apply false
-    id("uk.co.jasonmarston.project.standards.java-library") version "1.0.2" apply false
-    id("uk.co.jasonmarston.project.standards.quarkus-library") version "1.0.2" apply false
-    id("uk.co.jasonmarston.project.standards.quarkus-app") version "1.0.2" apply false
-    id("uk.co.jasonmarston.project.standards.liquibase") version "1.0.2" apply false
-    id("uk.co.jasonmarston.project.standards.version") version "1.0.2"
-    id("uk.co.jasonmarston.project.standards.gitops") version "1.0.2"
-}
-
-subprojects {
-    configurations.configureEach {
-        resolutionStrategy {
-            // Maven SNAPSHOTs are changing modules; 0s TTL forces metadata/artifact refresh checks.
-            cacheChangingModulesFor(0, "seconds")
-        }
-    }
+    id("uk.co.jasonmarston.project.standards.java-library") version "1.0.3" apply false
+    id("uk.co.jasonmarston.project.standards.quarkus-library") version "1.0.3" apply false
+    id("uk.co.jasonmarston.project.standards.quarkus-app") version "1.0.3" apply false
+    id("uk.co.jasonmarston.project.standards.liquibase") version "1.0.3" apply false
+    id("uk.co.jasonmarston.project.standards.version") version "1.0.3"
+    id("uk.co.jasonmarston.project.standards.gitops") version "1.0.3"
 }
 
 gitOpsPromote {
